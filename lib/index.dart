291a07f94eec3b6df25efc41fe1207f419dcdaf2
Jos-100 <delariva.martinez.hectorjosue@gmail.com>
2024-05-26 18:48:35 -0600

    Josue

---
+++ lib/index.dart
@@ -0,0 +1,31 @@
+// Export pages
+export '/hoid/gestion_eventos/gestion_eventos_widget.dart'
+    show GestionEventosWidget;
+export '/hoid/inicio_registro/inicio_registro_widget.dart'
+    show InicioRegistroWidget;
+export '/hoid/confirmacion_compra/confirmacion_compra_widget.dart'
+    show ConfirmacionCompraWidget;
+export '/hoid/detalles_evento/detalles_evento_widget.dart'
+    show DetallesEventoWidget;
+export '/hoid/carrito_compras/carrito_compras_widget.dart'
+    show CarritoComprasWidget;
+export '/hoid/pagina_inicio/pagina_inicio_widget.dart' show PaginaInicioWidget;
+export '/hoid/generacion_fan_i_d/generacion_fan_i_d_widget.dart'
+    show GeneracionFanIDWidget;
+export '/hoid/pagina_principal/pagina_principal_widget.dart'
+    show PaginaPrincipalWidget;
+export '/hoid/recomendaciones_evento/recomendaciones_evento_widget.dart'
+    show RecomendacionesEventoWidget;
+export '/hoid/salidadelevento/salidadelevento_widget.dart'
+    show SalidadeleventoWidget;
+export '/hoid/mis_eventos/mis_eventos_widget.dart' show MisEventosWidget;
+export '/hoid/fan_i_d_generation/fan_i_d_generation_widget.dart'
+    show FanIDGenerationWidget;
+export '/hoid/solicitud_boletos/solicitud_boletos_widget.dart'
+    show SolicitudBoletosWidget;
+export '/hoid/entrada_evento/entrada_evento_widget.dart'
+    show EntradaEventoWidget;
+export '/hoid/validacion_boleto/validacion_boleto_widget.dart'
+    show ValidacionBoletoWidget;
+export '/hoid/validacion_fan_id/validacion_fan_id_widget.dart'
+    show ValidacionFanIdWidget;
